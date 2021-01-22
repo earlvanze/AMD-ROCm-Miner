9f55a97849b1278091e3cc49d8c4f7636585c186
Earl Co <earlvanze@gmail.com>
2021-01-22 01:19:25 +0100

    13 GPUs

--- start-oc.sh
+++ start-oc.sh
@@ -12,25 +12,34 @@
 #find /sys | grep power_dpm_state
 #find /sys | grep power_dpm_force_performance_level
 
-echo performance >/sys/devices/pci0000:00/0000:00:1c.0/0000:02:00.0/power_dpm_state
-echo performance >/sys/devices/pci0000:00/0000:00:01.0/0000:01:00.0/power_dpm_state
-echo performance >/sys/devices/pci0000:00/0000:00:1d.1/0000:07:00.0/0000:08:04.0/0000:0c:00.0/power_dpm_state
-echo performance >/sys/devices/pci0000:00/0000:00:1d.1/0000:07:00.0/0000:08:07.0/0000:0f:00.0/power_dpm_state
-echo performance >/sys/devices/pci0000:00/0000:00:1d.1/0000:07:00.0/0000:08:03.0/0000:0b:00.0/power_dpm_state
-echo performance >/sys/devices/pci0000:00/0000:00:1d.1/0000:07:00.0/0000:08:02.0/0000:0a:00.0/power_dpm_state
-echo performance >/sys/devices/pci0000:00/0000:00:1c.7/0000:05:00.0/power_dpm_state
-echo performance >/sys/devices/pci0000:00/0000:00:1c.5/0000:03:00.0/power_dpm_state
-echo performance >/sys/devices/pci0000:00/0000:00:1c.6/0000:04:00.0/power_dpm_state
-
 echo manual >/sys/devices/pci0000:00/0000:00:1c.0/0000:02:00.0/power_dpm_force_performance_level
 echo manual >/sys/devices/pci0000:00/0000:00:01.0/0000:01:00.0/power_dpm_force_performance_level
 echo manual >/sys/devices/pci0000:00/0000:00:1d.1/0000:07:00.0/0000:08:04.0/0000:0c:00.0/power_dpm_force_performance_level
 echo manual >/sys/devices/pci0000:00/0000:00:1d.1/0000:07:00.0/0000:08:07.0/0000:0f:00.0/power_dpm_force_performance_level
 echo manual >/sys/devices/pci0000:00/0000:00:1d.1/0000:07:00.0/0000:08:03.0/0000:0b:00.0/power_dpm_force_performance_level
+echo manual >/sys/devices/pci0000:00/0000:00:1d.1/0000:07:00.0/0000:08:06.0/0000:0e:00.0/power_dpm_force_performance_level
 echo manual >/sys/devices/pci0000:00/0000:00:1d.1/0000:07:00.0/0000:08:02.0/0000:0a:00.0/power_dpm_force_performance_level
+echo manual >/sys/devices/pci0000:00/0000:00:1d.1/0000:07:00.0/0000:08:05.0/0000:0d:00.0/power_dpm_force_performance_level
+echo manual >/sys/devices/pci0000:00/0000:00:1d.1/0000:07:00.0/0000:08:01.0/0000:09:00.0/power_dpm_force_performance_level
 echo manual >/sys/devices/pci0000:00/0000:00:1c.7/0000:05:00.0/power_dpm_force_performance_level
 echo manual >/sys/devices/pci0000:00/0000:00:1c.5/0000:03:00.0/power_dpm_force_performance_level
+echo manual >/sys/devices/pci0000:00/0000:00:1d.0/0000:06:00.0/power_dpm_force_performance_level
 echo manual >/sys/devices/pci0000:00/0000:00:1c.6/0000:04:00.0/power_dpm_force_performance_level
+
+
+echo performance >/sys/devices/pci0000:00/0000:00:1c.0/0000:02:00.0/power_dpm_state
+echo performance >/sys/devices/pci0000:00/0000:00:01.0/0000:01:00.0/power_dpm_state
+echo performance >/sys/devices/pci0000:00/0000:00:1d.1/0000:07:00.0/0000:08:04.0/0000:0c:00.0/power_dpm_state
+echo performance >/sys/devices/pci0000:00/0000:00:1d.1/0000:07:00.0/0000:08:07.0/0000:0f:00.0/power_dpm_state
+echo performance >/sys/devices/pci0000:00/0000:00:1d.1/0000:07:00.0/0000:08:03.0/0000:0b:00.0/power_dpm_state
+echo performance >/sys/devices/pci0000:00/0000:00:1d.1/0000:07:00.0/0000:08:06.0/0000:0e:00.0/power_dpm_state
+echo performance >/sys/devices/pci0000:00/0000:00:1d.1/0000:07:00.0/0000:08:02.0/0000:0a:00.0/power_dpm_state
+echo performance >/sys/devices/pci0000:00/0000:00:1d.1/0000:07:00.0/0000:08:05.0/0000:0d:00.0/power_dpm_state
+echo performance >/sys/devices/pci0000:00/0000:00:1d.1/0000:07:00.0/0000:08:01.0/0000:09:00.0/power_dpm_state
+echo performance >/sys/devices/pci0000:00/0000:00:1c.7/0000:05:00.0/power_dpm_state
+echo performance >/sys/devices/pci0000:00/0000:00:1c.5/0000:03:00.0/power_dpm_state
+echo performance >/sys/devices/pci0000:00/0000:00:1d.0/0000:06:00.0/power_dpm_state
+echo performance >/sys/devices/pci0000:00/0000:00:1c.6/0000:04:00.0/power_dpm_state
 
 #Run the first command and then then it will give you the find command to run to
 #get the over drive paths
